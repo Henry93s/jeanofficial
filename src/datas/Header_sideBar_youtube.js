f5e6bd70328cce0eb4f231a65538a75f767c75f1
henry0903 <gudrjsdn8825@naver.com>
2024-07-01 03:39:20 +0900

    HTML entity 특수 문자 replace

--- src/datas/Header_sideBar_youtube.js
+++ src/datas/Header_sideBar_youtube.js
@@ -5,10 +5,10 @@ import config from './config.json';
 // react app 에서 env 사용할 때는 REACT_APP_ 추가하여야 함
 const API_KEY = config.YOUTUBE_API_KEY;
 const CHANNEL_ID = config.CHANNEL_ID;
-const youtube_data = [];
-const youtubeFetch = async () => {
-    
 
+const youtube_data = [];
+
+const youtubeFetch = async () => {
     await axios.get(`https://www.googleapis.com/youtube/v3/search`,{
         // 최신 동영상 top 10 동영상 fetch
         params: {
@@ -26,7 +26,11 @@ const youtubeFetch = async () => {
             const videoTitle = v.snippet.title;
             const thumbnailUrl = v.snippet.thumbnails.high.url;
             
-            console.log(`Video Title: ${videoTitle}`);
+            // &#39; 문자를 ' 문자로, &amp; 문자를 & 문자로 replace
+            let newVideo_title = videoTitle.replace(/&#39;/g, '\'');
+            newVideo_title = newVideo_title.replace(/&amp;/g, '&');
+
+            console.log(`Video Title: ${newVideo_title}`);
             console.log(`Thumbnail URL: ${thumbnailUrl}`);
             console.log(`Video ID: ${videoId}`);
             youtube_data.push(v);
